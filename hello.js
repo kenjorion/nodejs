61d62140c34a23868c9d00b829f24f7c6aa5327d
Ken <ken.mavoungou@gmail.com>
2019-09-23 12:30:39 +0200

    t

--- hello.js
+++ hello.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+//console.log("Hello World"); 
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
